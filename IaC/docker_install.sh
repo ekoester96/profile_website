a5ce197685844b637301b2ca89da8a586bb8501f
ekoester96 <ethan.schmidt1996@gmail.com>
2026-04-13 19:18:43 -0500

    add IaC folder and docker install script

---
+++ IaC/docker_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Install Docker
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add myself to docker group so I don't need sudo
+sudo usermod -aG docker $(whoami)
+
+# Log out and back in for group change to take effect
+exit
